a47db83494487e6faa12c39709d7508b904fd097
jerrygoh <jerrygjy@gmail.com>
2020-01-02 11:41:23 +0800

    Add Source

---
+++ OneMappy/OneMappy/OneMappy.h
@@ -0,0 +1,19 @@
+//
+//  OneMappy.h
+//  OneMappy
+//
+//  Created by Jerry Goh on 2/1/20.
+//  Copyright © 2020 Jerry Goh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for OneMappy.
+FOUNDATION_EXPORT double OneMappyVersionNumber;
+
+//! Project version string for OneMappy.
+FOUNDATION_EXPORT const unsigned char OneMappyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneMappy/PublicHeader.h>
+
+
---
+++ OneMappy/OneMappyExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OneMappyExample
+//
+//  Created by Jerry Goh on 2/1/20.
+//  Copyright © 2020 Jerry Goh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
